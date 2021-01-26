a368e9f789cf8b6b88f321ab11e8a3f022ec33e0
Kulwinder Rupal <kulwinder_rupal@yahoo.com>
2021-01-27 09:27:50 +1100

    Fixing decimal values

--- src/App.js
+++ src/App.js
@@ -23,19 +23,18 @@ function App() {
     setResultLabel(`${fromCurr} to ${toCurr}`)
     try {
       const result = convert(fromCurr, toCurr, amount);
-      const resRoundedTo2Dec = Math.round((result + Number.EPSILON) * 100) / 100;
-      setResult(resRoundedTo2Dec);
+      const precision = toCurr === 'JPY' ? 0 : 2;
+      setResult(result.toFixed(precision));
     } catch (err) {
       setError(`Unable to find rate for ${fromCurr}/${toCurr}`)
     }
-    
   }
   return (
     <div className="container">
       <h1>Currency Converter</h1>
       <div className="row">
         <div className="col-sm-4">
-          <form className="form-floating">  
+          <form className="form-floating">           
             <InputText type='text' label='From Currency' name='from' id='from' onChange={handleCurrencyChange.bind(this, 'FROM')}/>
             <InputText type='number' label='Amount' name='amount' id='amount' onChange={(e)=>setAmount(+e.target.value)}/>
             <InputText type='text' label='To Currency' name='to' id='to' onChange={handleCurrencyChange.bind(this, 'TO')}/>
