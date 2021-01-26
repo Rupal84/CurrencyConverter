656e1de2f9ee7716d0bb0a795af391e1a1030c38
Kulwinder Rupal <kulwinder_rupal@yahoo.com>
2021-01-27 10:24:48 +1100

    Adding validations

--- src/App.js
+++ src/App.js
@@ -1,51 +1,85 @@
 import { useState } from 'react'
-import InputText from './InputText'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import Result from './Result'
-import {convert} from './conversionTable'
+import { convert } from './conversionTable'
 
 function App() {
-  const [fromCurr, setFromCurr] = useState('');
-  const [toCurr, setToCurr] = useState('');
   const [resultLabel, setResultLabel] = useState();
-  const [amount, setAmount] = useState();
   const [result, setResult] = useState();
   const [error, setError] = useState();
-  const handleCurrencyChange = (currType, e) => {
-    const value = e.target.value;
-    if(currType === 'FROM') {
-      setFromCurr(value.toUpperCase());
-    } else {
-      setToCurr(value.toUpperCase());
-    }
-  }
-  const handleConversion = () => {
+
+  const handleConversion = ({fromCurr, toCurr, amount}) => {
     setError();
-    setResultLabel(`${fromCurr} to ${toCurr}`)
+    const fromCurrency = fromCurr.toUpperCase();
+    const toCurrency = toCurr.toUpperCase();    
+    setResultLabel(`${fromCurrency} to ${toCurrency}`)
     try {
-      const result = convert(fromCurr, toCurr, amount);
-      const precision = toCurr === 'JPY' ? 0 : 2;
+      const result = convert(fromCurrency, toCurrency, amount);
+      const precision = toCurrency === 'JPY' ? 0 : 2;
       setResult(result.toFixed(precision));
     } catch (err) {
-      setError(`Unable to find rate for ${fromCurr}/${toCurr}`)
+      setError(`Unable to find rate for ${fromCurrency}/${toCurrency}`)
     }
   }
   return (
-    <div className="container">
-      <h1>Currency Converter</h1>
-      <div className="row">
-        <div className="col-sm-4">
-          <form className="form-floating">           
-            <InputText type='text' label='From Currency' name='from' id='from' onChange={handleCurrencyChange.bind(this, 'FROM')}/>
-            <InputText type='number' label='Amount' name='amount' id='amount' onChange={(e)=>setAmount(+e.target.value)}/>
-            <InputText type='text' label='To Currency' name='to' id='to' onChange={handleCurrencyChange.bind(this, 'TO')}/>
-            <button type="button" className="btn btn-success" onClick={handleConversion}>Convert</button>
-          </form>
-        </div>
-        <div className="col-sm-4">
-           <Result label={resultLabel} error={error} result={result}/> 
-        </div>
-      </div>
-    </div>
+    <Formik
+      initialValues={{
+        fromCurr: '',
+        amount: '',
+        toCurr: ''
+      }}
+      validationSchema={Yup.object().shape({
+        fromCurr: Yup.string()
+          .required('From Currency is required'),
+        amount: Yup.number()
+          .required('Amount is required'),
+        toCurr: Yup.string()
+          .required('Email is required')
+      })}
+      onSubmit={fields => {
+        handleConversion(fields);
+      }}
+      render={({ errors, touched }) => (
+        <Form>
+          <div className="container">
+            <h1>Currency Converter</h1>
+            <div className="row">
+              <div className="col-sm-4">
+                <div className="form-group">
+                  <label htmlFor={'fromCurr'} className="form-label">From Currency</label>
+                  <Field type='text'
+                    name='fromCurr'
+                    className={'form-control' + (errors.fromCurr && touched.fromCurr ? ' is-invalid' : '')}
+                  />
+                  <ErrorMessage name='fromCurr' component="div" className="invalid-feedback" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor={'amount'} className="form-label">Amount</label>
+                  <Field type='number'
+                    name='amount'
+                    className={'form-control' + (errors.amount && touched.amount ? ' is-invalid' : '')}
+                  />
+                  <ErrorMessage name='amount' component="div" className="invalid-feedback" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor={'toCurr'} className="form-label">To Currency</label>
+                  <Field type='text'
+                    name='toCurr'
+                    className={'form-control' + (errors.toCurr && touched.toCurr ? ' is-invalid' : '')}
+                  />
+                  <ErrorMessage name='toCurr' component="div" className="invalid-feedback" />
+                </div>
+                <button type="submit" className="btn btn-success">Convert</button>
+              </div>
+              <div className="col-sm-4">
+                <Result label={resultLabel} error={error} result={result} />
+              </div>
+            </div>
+          </div>
+        </Form>
+      )}
+    />
   );
 }
 
--- src/Result.js
+++ src/Result.js
@@ -4,7 +4,7 @@ const Result = (props) => {
     return (
         <React.Fragment>
             {props.error ? 
-                <div className="alert alert-danger" role="alert">
+                <div className="alert alert-danger error" role="alert">
                     {props.error}
                 </div> : 
                 props.label && <div className="result">
